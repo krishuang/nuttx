db7166261bb3b6a57d3a3db0a65d91df0c17c0ab
Jason Hung <hung_jason@projectara.com>
2015-05-26 17:09:23 +0800

    greybus: spi: Add Greybus SPI protocol driver
    Implement Greybus SPI protocol driver, and add SPI CPORT routing on SVC,
    also provided the manifest file for SPI module. Tested on BDB1B board.
    
    Testing Done: received greybus SPI operation from AP Kernel side, and feedback response data
    Bug Number: SW-648
    Review URL:
    Reviewed by:

--- apps/greybus-utils/manifest.c
+++ apps/greybus-utils/manifest.c
@@ -49,6 +49,7 @@ extern void gb_i2c_register(int cport);
 extern void gb_battery_register(int cport);
 extern void gb_loopback_register(int cport);
 extern void gb_vibrator_register(int cport);
+extern void gb_spi_register(int cport);
 
 struct greybus {
     /* TODO use a list instead */
@@ -128,6 +129,13 @@ void enable_cports(void)
             if (protocol == GREYBUS_PROTOCOL_VIBRATOR) {
                 gb_info("Registering VIBRATOR greybus driver.\n");
                 gb_vibrator_register(id);
+            }
+#endif
+
+#ifdef CONFIG_GREYBUS_SPI_PHY
+            if (protocol == GREYBUS_PROTOCOL_SPI) {
+                gb_info("Registering SPI greybus driver.\n");
+                gb_spi_register(id);
             }
 #endif
         }
