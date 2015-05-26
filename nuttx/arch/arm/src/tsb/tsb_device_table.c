c138954a81e6394ab430b7c51addb5a2a2fa32b5
Jason Hung <hung_jason@projectara.com>
2015-05-26 16:04:25 +0800

    tsb: spi: Add TSB SPI device driver
    Add the dummy device driver for SPI with device core interface supported.
    Because SPI master only supported by Toshiba ES3 chip, so we create a
    dummy SPI device driver for Greybus SPI protocol testing.
    
    Testing Done: received SPI request from protocol driver adn return testing data
    Bug Number: SW-648
    Review URL:
    Reviewed by:

--- nuttx/arch/arm/src/tsb/tsb_device_table.c
+++ nuttx/arch/arm/src/tsb/tsb_device_table.c
@@ -33,6 +33,7 @@
 #include <nuttx/device_resource.h>
 #include <nuttx/device_table.h>
 #include <nuttx/device_pll.h>
+#include <nuttx/device_spi.h>
 
 #include "chip.h"
 
@@ -50,6 +51,17 @@ static struct device_resource tsb_plla_resources[] = {
 };
 #endif
 
+#ifdef CONFIG_ARCH_CHIP_DEVICE_SPI
+static struct device_resource tsb_spi_resources[] = {
+    {
+        .name   = "reg_base",
+        .type   = DEVICE_RESOUCE_TYPE_REGS,
+        .start  = SPI_BASE,
+        .count  = SPI_SIZE,
+    },
+};
+#endif
+
 static struct device tsb_device_table[] = {
 #ifdef CONFIG_ARCH_CHIP_DEVICE_PLL
     {
@@ -59,6 +71,16 @@ static struct device tsb_device_table[] = {
         .id             = 0,
         .resources      = tsb_plla_resources,
         .resource_count = ARRAY_SIZE(tsb_plla_resources),
+    },
+#endif
+#ifdef CONFIG_ARCH_CHIP_DEVICE_SPI
+    {
+        .type           = DEVICE_TYPE_SPI_HW,
+        .name           = "tsb_spi",
+        .desc           = "TSB SPI master Controller",
+        .id             = 0,
+        .resources      = tsb_spi_resources,
+        .resource_count = ARRAY_SIZE(tsb_spi_resources),
     },
 #endif
 };
